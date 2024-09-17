db25f4f2a7580865e283be88cf94af455b139cb6
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-09-17 07:08:25 -0500

    unicon con pc

--- backend/app/Http/Controllers/API/TipoNegocioController.php
+++ backend/app/Http/Controllers/API/TipoNegocioController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\TipoNegocio;
+
 
 class TipoNegocioController extends Controller
 {
