820028840e8ed79c94cd962b55433b6afb1eb17b
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-03 01:18:51 -0500

    modificacion del servicio auth

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:8000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  register(userData: {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+  }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
 }
