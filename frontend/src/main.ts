9f4b039cd14fc519976fbe565b7741a3d5ccf2df
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-05 00:12:15 -0500

    registro del provide y del interceptor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,10 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { cartOutline, constructOutline, helpCircle, homeOutline, locationOutline, personOutline, restaurantOutline, searchOutline, storefrontOutline, wineOutline, restaurant, wine, cart, construct, logInOutline, personAddOutline, } from 'ionicons/icons';
+import { cartOutline, constructOutline, helpCircle, homeOutline, locationOutline, personOutline, restaurantOutline, searchOutline, storefrontOutline, wineOutline, restaurant, wine, cart, construct, logInOutline, personAddOutline, trashOutline, logOutOutline, } from 'ionicons/icons';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 addIcons(
@@ -26,15 +26,17 @@ addIcons(
     'help-circle': helpCircle,
     'log-in-outline': logInOutline,
     'person-add-outline': personAddOutline,
+    'trash-outline': trashOutline,
+    'log-out-outline': logOutOutline,
     
   }
 );
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(withInterceptors([authInterceptor])),
+  
   ],
 });
