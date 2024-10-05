ca82be24abd36323c84b5f41ca5a348446ea4c60
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-05 00:08:15 -0500

    agregamos los campos al fillable

--- backend/app/Models/CarritoItem.php
+++ backend/app/Models/CarritoItem.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CarritoItem extends Model
 {
+    // Agrega las propiedades que pueden ser asignadas en masa
+    protected $fillable = [
+        'carrito_id',
+        'producto_id',
+        'cantidad',
+        'precio_unitario',
+        'total',
+    ];
     use HasFactory;
     public function carrito()
     {
