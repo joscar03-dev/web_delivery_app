9f7e854cb905769e4a67c5c7a3834edd5290befa
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-30 06:16:53 -0500

    cambio en la ruta principal

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/',function () {
+    return redirect('/admin');
+    }
+);
