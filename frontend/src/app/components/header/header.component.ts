fa3d432e967a4341428a0de51d658681eba18c38
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-30 06:20:37 -0500

    header component

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [
-    IonicModule
-  ]
+  imports: [IonicModule],
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  userLocation: string = '';
 
-  constructor() { }
+  constructor() {}
 
-
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          this.userLocation = `Latitud: ${lat}, Longitud: ${lng}`;
+        },
+        (error) => {
+          console.error('Error obteniendo la ubicación', error);
+          this.userLocation = 'No se pudo obtener la ubicación';
+        }
+      );
+    } else {
+      this.userLocation = 'Geolocalización no soportada por el navegador';
+    }
+  }
 }
