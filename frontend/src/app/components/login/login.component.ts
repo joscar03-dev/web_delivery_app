5ac46dd97b39717951e15fd5950355e83f66cd18
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-30 06:20:01 -0500

    login sin comentarios

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,27 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonicModule] 
+  imports: [CommonModule, FormsModule, IonicModule],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService) {}
+  // authServices = inject(AuthService); // Nueva forma de inyectar dependencias en angular 18
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   login() {
     this.authService.login(this.email, this.password).subscribe({
       next: (res) => {
         localStorage.setItem('token', res.token);
-        // Redirigir o realizar alguna acción post-login
+        this.router.navigate(['/home']);
       },
       error: (err) => {
         console.error(err);
