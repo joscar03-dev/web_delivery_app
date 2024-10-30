4c13513816747ead4b04a8731d39eb9e28567cd8
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-30 06:18:13 -0500

    guard modificado

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -2,12 +2,12 @@ import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const token = localStorage.getItem('token');
-  
+
   if (token) {
-    return true; // Permitir acceso si el token está presente (usuario autenticado)
+    return true;
   } else {
-    const router = new Router(); // Crear una instancia de Router para redirigir
-    router.navigate(['/login']); // Redirigir a la página de login si no está autenticado
-    return false; // No permitir acceso
+    const router = new Router();
+    router.navigate(['/login']);
+    return false;
   }
 };
